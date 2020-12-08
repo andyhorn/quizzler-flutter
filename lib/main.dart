3408a03c8fdabd20364f4870f674c59d5671d915
Andy <andyjhorn@gmail.com>
2020-12-08 13:56:37 -0600

    Finished exercise

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Question.dart';
+
 void main() => runApp(Quizzler());
 
 class Quizzler extends StatelessWidget {
@@ -25,6 +27,18 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int questionNumber = 0;
+  List<Icon> scores = [];
+  List<Question> questions = [
+    Question(
+        question: "You can lead a cow down stairs but not up stairs.",
+        answer: false),
+    Question(
+        question: "Approximately one quarter of human bones are in the feet.",
+        answer: true),
+    Question(question: "A slug\'s blood is green.", answer: true)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +51,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber].question,
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -62,6 +76,17 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked true.
+                setState(() {
+                  if (questions[questionNumber].answer) {
+                    scores.add(Icon(Icons.check, color: Colors.green));
+                  } else {
+                    scores.add(Icon(Icons.warning, color: Colors.red));
+                  }
+
+                  if (questionNumber < questions.length - 1)
+                    questionNumber++;
+                  // questionNumber++;
+                });
               },
             ),
           ),
@@ -80,11 +105,24 @@ class _QuizPageState extends State<QuizPage> {
               ),
               onPressed: () {
                 //The user picked false.
+                setState(() {
+                  if (questions[questionNumber++].answer == false) {
+                    scores.add(Icon(Icons.check, color: Colors.green));
+                  } else {
+                    scores.add(
+                        Icon(Icons.warning_amber_outlined, color: Colors.red));
+                  }
+
+                  if (questionNumber < questions.length - 1)
+                    questionNumber++;
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scores,
+        )
       ],
     );
   }
